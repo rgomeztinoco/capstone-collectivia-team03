db968f0f3b7e0ea50d5672e1db0b9d2f77b898e0
rgomeztinoco <gricardo2213@gmail.com>
2022-04-12 15:38:09 -0500

    controller logic

--- Gemfile
+++ Gemfile
@@ -48,12 +48,15 @@ gem "sassc-rails"
 # Use Haml to process HTML
 gem "haml"
 
+gem "httparty", "~> 0.18.1"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
+  gem "dotenv-rails"
 end
 
 group :development do
@@ -62,6 +65,8 @@ group :development do
 
   gem "rubocop", require: false
   gem "rubocop-rails", require: false
+
+  gem "i18n-tasks", "~> 1.0.9"
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,49 @@
 class UsersController < ApplicationController
   def new
-    @user = "user"
+    @user = User.new
   end
 
   def edit
-
+    @user = User.find_by(email: params[:email])
   end
 
   def create
-    
+    @user = User.new(user_params)
+    response = email_verification_request(@user.email)
+    if response.quality_score.to_i >= 0.7
+      if @user.save
+        render success_page
+      else
+        render error_page
+      end
+    else
+      render error_page
+    end
   end
 
   def update
-    
+    @user = User.find_by(email: params[:email])
+
+    if @user.update(user_params)
+      render success_page
+    else
+      render error_page
+    end
+  end
+
+  private
+
+  def email_verification_request(email)
+    base_uri = "https://emailvalidation.abstractapi.com/v1/"
+    query = {
+      api_key: ENV.EMAIL_VALIDATION_API_KEY,
+      email: email
+    }
+    response = HTTParty.get(base_uri, query: query)
+    JSON.parse(response.body)
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
