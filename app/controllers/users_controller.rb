ffbcee700c024e3ad552925a3c37353bacdfc2b4
rgomeztinoco <gricardo2213@gmail.com>
2022-04-12 21:24:05 -0500

    ✨ user controllers and views. error handling.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,65 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def show; end
+
   def new
     @user = User.new
   end
 
-  def edit
-    @user = User.find_by(email: params[:email])
-  end
-
   def create
     @user = User.new(user_params)
-    response = email_verification_request(@user.email)
-    if response.quality_score.to_i >= 0.7
-      if @user.save
+
+    if @user.validate
+      response = email_verification_request(@user.email)
+      if response[:quality_score].to_f >= 0.7
+        @user.save
         render success_page
       else
-        render error_page
+        @user.errors.add(:email, "this email doesn't seem to be real")
+        render :new, status: :unprocessable_entity
       end
     else
-      render error_page
+      render :new, status: :unprocessable_entity
     end
+  rescue HTTParty::ResponseError => e
+    flash[:alert] = JSON.parse(e.message)["error"]["message"]
+    render :new, status: :unprocessable_entity
   end
 
-  def update
-    @user = User.find_by(email: params[:email])
+  def edit; end
 
+  def update
     if @user.update(user_params)
       render success_page
     else
-      render error_page
+      render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy; end
 
   private
 
   def email_verification_request(email)
     base_uri = "https://emailvalidation.abstractapi.com/v1/"
     query = {
-      api_key: ENV.EMAIL_VALIDATION_API_KEY,
+      api_key: ENV["EMAIL_VALIDATION_API_KEY"],
       email: email
     }
     response = HTTParty.get(base_uri, query: query)
-    JSON.parse(response.body)
+    raise(HTTParty::ResponseError, response) unless response.success?
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 
+  # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, topic_ids: [])
   end
 
-  def index
-    
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
   end
 end
